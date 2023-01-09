5262945e412483a3ac96a766eb380f7dcc8728d8
Joao <joao@truecompliance.co.uk>
2023-01-09 14:24:56 +0000

    chore: update vite file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vite-ts-vue-class-composition-boiler-plate/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
